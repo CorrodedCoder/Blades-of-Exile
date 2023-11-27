4c368b93db9ef23fd77c1e696c3e3312f17456f5
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-11-27 09:14:40 +0000

    handle_keystroke should return a value
    But it does not... For now add an assert and return FALSE.

--- src/Windows Code Release 3/Scen Ed/tactions.cpp
+++ src/Windows Code Release 3/Scen Ed/tactions.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <cassert>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -1351,6 +1352,9 @@ Boolean handle_keystroke(UINT wParam,LONG lParam)
 	if (store_ter != current_terrain_type)
 		draw_terrain();
 	mouse_button_held = FALSE;
+	// This function expects to return a value, but nothing is being returned
+	assert(false);
+	return FALSE;
 }
 
 
