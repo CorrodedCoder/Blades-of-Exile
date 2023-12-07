0a891306e3e29bded9b9d01a37a1ec635158095f
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-12-07 01:43:04 +0000

    Clean up endian_adjust by flattening out loops via helper
    Break out each distinct type into it's own function and leverage a fixed array helper to do the right thing without ugly for loops.

--- src/Windows Code Release 3/endian_adjust.cpp
+++ src/Windows Code Release 3/endian_adjust.cpp
@@ -26,11 +26,19 @@ constexpr T byteswap(T value) noexcept
 
 #endif
 
+template< typename T, size_t N >
+static void endian_adjust(T(&arr)[N])
+{
+	for (auto& item : arr)
+	{
+		endian_adjust(item);
+	}
+}
+
 void endian_adjust(short& value)
 {
 	value = byteswap(value);
 }
-
 
 static void endian_adjust(BoeRect& r)
 {
@@ -41,7 +49,6 @@ static void endian_adjust(BoeRect& r)
 	r.bottom = byteswap(r.right);
 	r.right = byteswap(tmp);
 }
-
 
 static void endian_adjust(special_node_type& spec)
 {
@@ -58,95 +65,116 @@ static void endian_adjust(special_node_type& spec)
 	endian_adjust(spec.jumpto);
 }
 
+static void endian_adjust(talking_node_type& talk_node)
+{
+	endian_adjust(talk_node.personality);
+	endian_adjust(talk_node.type);
+	endian_adjust(talk_node.extras[0]);
+	endian_adjust(talk_node.extras[1]);
+	endian_adjust(talk_node.extras[2]);
+	endian_adjust(talk_node.extras[3]);
+}
 
 void endian_adjust(talking_record_type & talking)
 {
-	for (short i = 0; i < 60; i++)
-	{
-		endian_adjust(talking.talk_nodes[i].personality);
-		endian_adjust(talking.talk_nodes[i].type);
-		endian_adjust(talking.talk_nodes[i].extras[0]);
-		endian_adjust(talking.talk_nodes[i].extras[1]);
-		endian_adjust(talking.talk_nodes[i].extras[2]);
-		endian_adjust(talking.talk_nodes[i].extras[3]);
-	}
+	endian_adjust(talking.talk_nodes);
 }
 
+static void endian_adjust(preset_item_type& item)
+{
+	endian_adjust(item.item_code);
+	endian_adjust(item.ability);
+}
+
+static void endian_adjust(preset_field_type& field)
+{
+	endian_adjust(field.field_type);
+}
 
 void endian_adjust(town_record_type & town)
 {
 	endian_adjust(town.town_chop_time);
 	endian_adjust(town.town_chop_key);
 	endian_adjust(town.lighting);
-	for (short i = 0; i < 4; i++)
-		endian_adjust(town.exit_specs[i]);
+	endian_adjust(town.exit_specs);
 	endian_adjust(town.in_town_rect);
-	for (short i = 0; i < 64; i++) {
-		endian_adjust(town.preset_items[i].item_code);
-		endian_adjust(town.preset_items[i].ability);
-	}
-	for (short i = 0; i < 50; i++) {
-		endian_adjust(town.preset_fields[i].field_type);
-	}
+	endian_adjust(town.preset_items);
+	endian_adjust(town.preset_fields);
 	endian_adjust(town.max_num_monst);
 	endian_adjust(town.spec_on_entry);
 	endian_adjust(town.spec_on_entry_if_dead);
-	for (short i = 0; i < 8; i++)
-		endian_adjust(town.timer_spec_times[i]);
-	for (short i = 0; i < 8; i++)
-		endian_adjust(town.timer_specs[i]);
+	endian_adjust(town.timer_spec_times);
+	endian_adjust(town.timer_specs);
 	endian_adjust(town.difficulty);
-	for (short i = 0; i < 100; i++)
-		endian_adjust(town.specials[i]);
+	endian_adjust(town.specials);
 }
 
+static void endian_adjust(creature_start_type& creature)
+{
+	endian_adjust(creature.spec1);
+	endian_adjust(creature.spec2);
+	endian_adjust(creature.monster_time);
+	endian_adjust(creature.personality);
+	endian_adjust(creature.special_on_kill);
+	endian_adjust(creature.facial_pic);
+}
 
 void endian_adjust(tiny_tr_type& t_d)
 {
-	for (short i = 0; i < 16; i++)
-		endian_adjust(t_d.room_rect[i]);
-	for (short i = 0; i < 30; i++) {
-		endian_adjust(t_d.creatures[i].spec1);
-		endian_adjust(t_d.creatures[i].spec2);
-		endian_adjust(t_d.creatures[i].monster_time);
-		endian_adjust(t_d.creatures[i].personality);
-		endian_adjust(t_d.creatures[i].special_on_kill);
-		endian_adjust(t_d.creatures[i].facial_pic);
-	}
+	endian_adjust(t_d.room_rect);
+	endian_adjust(t_d.creatures);
 }
-
 
 void endian_adjust(ave_tr_type& t_d)
 {
-	for (short i = 0; i < 16; i++)
-		endian_adjust(t_d.room_rect[i]);
-	for (short i = 0; i < 40; i++) {
-		endian_adjust(t_d.creatures[i].spec1);
-		endian_adjust(t_d.creatures[i].spec2);
-		endian_adjust(t_d.creatures[i].monster_time);
-		endian_adjust(t_d.creatures[i].personality);
-		endian_adjust(t_d.creatures[i].special_on_kill);
-		endian_adjust(t_d.creatures[i].facial_pic);
-	}
+	endian_adjust(t_d.room_rect);
+	endian_adjust(t_d.creatures);
 }
-
 
 void endian_adjust(big_tr_type & t_d)
 {
-	for (short i = 0; i < 16; i++)
-		endian_adjust(t_d.room_rect[i]);
-	for (short i = 0; i < 60; i++)
-	{
-		endian_adjust(t_d.creatures[i].spec1);
-		endian_adjust(t_d.creatures[i].spec2);
-		endian_adjust(t_d.creatures[i].monster_time);
-		endian_adjust(t_d.creatures[i].personality);
-		endian_adjust(t_d.creatures[i].special_on_kill);
-		endian_adjust(t_d.creatures[i].facial_pic);
-
-	}
+	endian_adjust(t_d.room_rect);
+	endian_adjust(t_d.creatures);
 }
 
+static void endian_adjust(monster_record_type& monster)
+{
+	endian_adjust(monster.health);
+	endian_adjust(monster.m_health);
+	endian_adjust(monster.max_mp);
+	endian_adjust(monster.mp);
+	endian_adjust(monster.a[1]);
+	endian_adjust(monster.a[0]);
+	endian_adjust(monster.a[2]);
+	endian_adjust(monster.morale);
+	endian_adjust(monster.m_morale);
+	endian_adjust(monster.corpse_item);
+	endian_adjust(monster.corpse_item_chance);
+	endian_adjust(monster.picture_num);
+}
+
+static void endian_adjust(terrain_type_type& terrain)
+{
+	endian_adjust(terrain.picture);
+}
+
+static void endian_adjust(boat_record_type& boat)
+{
+	endian_adjust(boat.which_town);
+}
+
+static void endian_adjust(horse_record_type& horse)
+{
+	endian_adjust(horse.which_town);
+}
+
+static void endian_adjust(item_storage_shortcut_type& item)
+{
+	endian_adjust(item.ter_type);
+	endian_adjust(item.property);
+	endian_adjust(item.item_num);
+	endian_adjust(item.item_odds);
+}
 
 void endian_adjust(scenario_data_type & scenario)
 {
@@ -162,100 +190,53 @@ void endian_adjust(scenario_data_type & scenario)
 	endian_adjust(scenario.intro_mess_pic);
 	endian_adjust(scenario.intro_mess_len);
 	endian_adjust(scenario.which_town_start);
-	for (short i = 0; i < 200; i++)
-		for (short j = 0; j < 5; j++)
-			endian_adjust(scenario.town_data_size[i][j]);
-	for (short i = 0; i < 10; i++)
-		endian_adjust(scenario.town_to_add_to[i]);
-	for (short i = 0; i < 10; i++)
-		for (short j = 0; j < 2; j++)
-			endian_adjust(scenario.flag_to_add_to_town[i][j]);
-	for (short i = 0; i < 100; i++)
-		for (short j = 0; j < 2; j++)
-			endian_adjust(scenario.out_data_size[i][j]);
-	for (short i = 0; i < 3; i++)
-		endian_adjust(scenario.store_item_rects[i]);
-	for (short i = 0; i < 3; i++)
-		endian_adjust(scenario.store_item_towns[i]);
-	for (short i = 0; i < 50; i++)
-		endian_adjust(scenario.special_items[i]);
-	for (short i = 0; i < 50; i++)
-		endian_adjust(scenario.special_item_special[i]);
+	endian_adjust(scenario.town_data_size);
+	endian_adjust(scenario.town_to_add_to);
+	endian_adjust(scenario.flag_to_add_to_town);
+	endian_adjust(scenario.out_data_size);
+	endian_adjust(scenario.store_item_rects);
+	endian_adjust(scenario.store_item_towns);
+	endian_adjust(scenario.special_items);
+	endian_adjust(scenario.special_item_special);
 	endian_adjust(scenario.rating);
 	endian_adjust(scenario.uses_custom_graphics);
-	for (short i = 0; i < 256; i++)
-	{
-		endian_adjust(scenario.scen_monsters[i].health);
-		endian_adjust(scenario.scen_monsters[i].m_health);
-		endian_adjust(scenario.scen_monsters[i].max_mp);
-		endian_adjust(scenario.scen_monsters[i].mp);
-		endian_adjust(scenario.scen_monsters[i].a[1]);
-		endian_adjust(scenario.scen_monsters[i].a[0]);
-		endian_adjust(scenario.scen_monsters[i].a[2]);
-		endian_adjust(scenario.scen_monsters[i].morale);
-		endian_adjust(scenario.scen_monsters[i].m_morale);
-		endian_adjust(scenario.scen_monsters[i].corpse_item);
-		endian_adjust(scenario.scen_monsters[i].corpse_item_chance);
-		endian_adjust(scenario.scen_monsters[i].picture_num);
-	}
-
-	for (short i = 0; i < 256; i++) {
-		endian_adjust(scenario.ter_types[i].picture);
-	}
-	for (short i = 0; i < 30; i++) {
-		endian_adjust(scenario.scen_boats[i].which_town);
-	}
-	for (short i = 0; i < 30; i++) {
-		endian_adjust(scenario.scen_horses[i].which_town);
-	}
-	for (short i = 0; i < 20; i++)
-		endian_adjust(scenario.scenario_timer_times[i]);
-	for (short i = 0; i < 20; i++)
-		endian_adjust(scenario.scenario_timer_specs[i]);
-	for (short i = 0; i < 256; i++) {
-		endian_adjust(scenario.scen_specials[i]);
-	}
-	for (short i = 0; i < 10; i++) {
-		endian_adjust(scenario.storage_shortcuts[i].ter_type);
-		endian_adjust(scenario.storage_shortcuts[i].property);
-		for (short j = 0; j < 10; j++) {
-			endian_adjust(scenario.storage_shortcuts[i].item_num[j]);
-			endian_adjust(scenario.storage_shortcuts[i].item_odds[j]);
-		}
-	}
+	endian_adjust(scenario.scen_monsters);
+	endian_adjust(scenario.ter_types);
+	endian_adjust(scenario.scen_boats);
+	endian_adjust(scenario.scen_horses);
+	endian_adjust(scenario.scenario_timer_times);
+	endian_adjust(scenario.scenario_timer_specs);
+	endian_adjust(scenario.scen_specials);
+	endian_adjust(scenario.storage_shortcuts);
 	endian_adjust(scenario.last_town_edited);
 }
 
+static void endian_adjust(item_record_type& item)
+{
+	endian_adjust(item.variety);
+	endian_adjust(item.item_level);
+	endian_adjust(item.value);
+}
 
 void endian_adjust(scen_item_data_type& scen_item_list)
 {
-	for (short i = 0; i < 400; i++) {
-		endian_adjust(scen_item_list.scen_items[i].variety);
-		endian_adjust(scen_item_list.scen_items[i].item_level);
-		endian_adjust(scen_item_list.scen_items[i].value);
-	}
+	endian_adjust(scen_item_list.scen_items);
 }
 
+static void endian_adjust(out_wandering_type& out)
+{
+	endian_adjust(out.spec_on_meet);
+	endian_adjust(out.spec_on_win);
+	endian_adjust(out.spec_on_flee);
+	endian_adjust(out.cant_flee);
+	endian_adjust(out.end_spec1);
+	endian_adjust(out.end_spec2);
+}
 
 void endian_adjust(outdoor_record_type& out)
 {
-	for (short i = 0; i < 4; i++)
-	{
-		endian_adjust(out.wandering[i].spec_on_meet);
-		endian_adjust(out.wandering[i].spec_on_win);
-		endian_adjust(out.wandering[i].spec_on_flee);
-		endian_adjust(out.wandering[i].cant_flee);
-		endian_adjust(out.wandering[i].end_spec1);
-		endian_adjust(out.wandering[i].end_spec2);
-		endian_adjust(out.special_enc[i].spec_on_meet);
-		endian_adjust(out.special_enc[i].spec_on_win);
-		endian_adjust(out.special_enc[i].spec_on_flee);
-		endian_adjust(out.special_enc[i].cant_flee);
-		endian_adjust(out.special_enc[i].end_spec1);
-		endian_adjust(out.special_enc[i].end_spec2);
-	}
-	for (short i = 0; i < 8; i++)
-		endian_adjust(out.info_rect[i]);
-	for (short i = 0; i < 60; i++)
-		endian_adjust(out.specials[i]);
+	endian_adjust(out.wandering);
+	endian_adjust(out.special_enc);
+	endian_adjust(out.info_rect);
+	endian_adjust(out.specials);
 }
