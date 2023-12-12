1a838fe9b3ef51d453af0d3ff3a6e86c29ebcb00
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-12-12 22:58:55 +0000

    Make graphutl.h common across all three incarnations

--- src/Windows Code Release 3/PC editor/graphutl.h
+++ src/Windows Code Release 3/PC editor/graphutl.h
@@ -2,7 +2,7 @@ void inflict_palette();
 void reset_palette();
 BYTE * GetDibBitsAddr(BYTE * lpDib);
 HBITMAP ReadDib(const char * name,HDC hdc);
-HBITMAP load_pict(short pict_num,HDC model_hdc);
+HBITMAP load_pict(short pict_num, HDC model_hdc);
 void rect_draw_some_item(HBITMAP src,RECT src_rect,HBITMAP dest,RECT dest_rect,short trans, short main_win);
 void fry_dc(HWND hwnd,HDC dc);
 void DisposeGWorld(HBITMAP bitmap);
--- src/Windows Code Release 3/graphutl.h
+++ src/Windows Code Release 3/graphutl.h
@@ -2,9 +2,8 @@ void inflict_palette();
 void reset_palette();
 BYTE * GetDibBitsAddr(BYTE * lpDib);
 HBITMAP ReadDib(const char * name,HDC hdc);
+HBITMAP load_pict(short pict_num, HDC model_hdc);
 void rect_draw_some_item(HBITMAP src,RECT src_rect,HBITMAP dest,RECT dest_rect,short trans, short main_win);
 void fry_dc(HWND hwnd,HDC dc);
 void DisposeGWorld(HBITMAP bitmap);
-HBITMAP load_pict(short pict_num,HDC model_hdc);
 void paint_pattern(HBITMAP dest,short which_mode,RECT dest_rect,short which_pattern);
-
