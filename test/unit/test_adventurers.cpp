4e832a2a5a8947697444a8a35d8394de7a8c2110
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-12-18 14:59:15 +0000

    Re-org test a little to please Visual Studio et al
    Ripple the enable_tests up to the top level so that it can be recognised by a run of CTest at any level (that was complicated to work out!).
    Also add to and improve the quality of the adventurer tests.

--- test/unit/test_adventurers.cpp
+++ test/unit/test_adventurers.cpp
@@ -4,14 +4,119 @@
 #include "boe/adventurers.hpp"
 
 
-TEST_CASE("Cave lore present", "[adventurers]")
+TEST_CASE("cave_lore_present", "[adventurers]")
 {
-	Adventurers adventurers{};
-	REQUIRE(!cave_lore_present(adventurers));
-	for (auto& adventurer : adventurers) { adventurer.main_status = 1; }
-	REQUIRE(!cave_lore_present(adventurers));
-	for (auto& adventurer : adventurers) { adventurer.main_status = 1; adventurer.traits[4] = 1; }
-	REQUIRE(cave_lore_present(adventurers));
-	for (auto& adventurer : adventurers) { adventurer.main_status = 0; }
-	REQUIRE(!cave_lore_present(adventurers));
+	{
+		Adventurers adventurers{};
+		REQUIRE(!cave_lore_present(adventurers));
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			REQUIRE(!cave_lore_present(adventurers));
+			adventurer.main_status = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			adventurer.traits[4] = 1;
+			REQUIRE(cave_lore_present(adventurers));
+			adventurer.main_status = 0;
+			adventurer.traits[4] = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.traits[4] = 1;
+			REQUIRE(!cave_lore_present(adventurers));
+			adventurer.traits[4] = 0;
+		}
+	}
+}
+
+TEST_CASE("woodsman_present", "[adventurers]")
+{
+	{
+		Adventurers adventurers{};
+		REQUIRE(!woodsman_present(adventurers));
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			REQUIRE(!woodsman_present(adventurers));
+			adventurer.main_status = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			adventurer.traits[5] = 1;
+			REQUIRE(woodsman_present(adventurers));
+			adventurer.main_status = 0;
+			adventurer.traits[5] = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.traits[5] = 1;
+			REQUIRE(!woodsman_present(adventurers));
+			adventurer.traits[5] = 0;
+		}
+	}
+}
+
+TEST_CASE("mage_lore_total", "[adventurers]")
+{
+	{
+		Adventurers adventurers{};
+		REQUIRE(mage_lore_total(adventurers) == 0);
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			REQUIRE(mage_lore_total(adventurers) == 0);
+			adventurer.main_status = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers)
+		{
+			adventurer.main_status = 1;
+			adventurer.skills[11] = 1;
+			REQUIRE(mage_lore_total(adventurers) == 1);
+			adventurer.main_status = 0;
+			adventurer.skills[11] = 0;
+		}
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers) { adventurer.skills[11] = 1; }
+		REQUIRE(mage_lore_total(adventurers) == 0);
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers) { adventurer.main_status = 1; adventurer.skills[11] = 1; }
+		REQUIRE(mage_lore_total(adventurers) == 6);
+	}
+	{
+		Adventurers adventurers{};
+		for (auto& adventurer : adventurers) { adventurer.main_status = 1; adventurer.skills[11] = 2; }
+		REQUIRE(mage_lore_total(adventurers) == 12);
+	}
 }
