5366bf3c0c1f44157333b0fd9defc7cc829bd854
CorrodedCoder <38778644+CorrodedCoder@users.noreply.github.com>
2023-12-27 18:01:49 +0000

    Have most item accessors to return by const reference

--- src/Windows Code Release 3/itemdata.cpp
+++ src/Windows Code Release 3/itemdata.cpp
@@ -6,12 +6,13 @@
 
 static const short loot_min[5] = {0,0,5,50,400};
 static const short loot_max[5] = {3,8,40,800,4000};
-//// whole file
 
-item_record_type get_stored_item(short which)
+static const item_record_type null_item{};
+
+const item_record_type& get_stored_item(short which)
 {
 	if ((which >= 400) || (which < 0)) {
-		return item_record_type{};
+		return null_item;
 	}
 	return scenario_item(which);
 }
@@ -33,98 +34,100 @@ item_record_type get_food()
 }
 
 
-item_record_type pull_item_of_type(short loot_max,short min_val,short max_val, item_variety t1, item_variety t2, item_variety t3)
+const item_record_type& pull_item_of_type(short loot_max,short min_val,short max_val, item_variety t1, item_variety t2, item_variety t3)
 {
-	short i,j,val;
-	item_record_type temp_i;
-	
 	// occasionally get nice item
-	if (get_ran(1,0,160) == 80) {
+	if (get_ran(1,0,160) == 80)
+	{
 		loot_max += 2;
 		max_val += 2000;
-		}
-	for (i = 0; i < 80; i++) {
-		j = get_ran(1,0,399);
-		temp_i = get_stored_item(j);
-		if ((temp_i.variety == t1) || (temp_i.variety == t2) || (temp_i.variety == t3)) {
-			val = (temp_i.charges > 0) ? temp_i.charges * temp_i.value : temp_i.value;
+	}
+	for (short i = 0; i < 80; i++)
+	{
+		const auto j = get_ran(1,0,399);
+		const auto& temp_i = get_stored_item(j);
+		if ((temp_i.variety == t1) || (temp_i.variety == t2) || (temp_i.variety == t3))
+		{
+			const short val = (temp_i.charges > 0) ? temp_i.charges * temp_i.value : temp_i.value;
 			if ((val >= min_val) && (val <= max_val) && (temp_i.treas_class != 0) &&
 				(temp_i.treas_class <= loot_max))
+			{
 				return temp_i;
 			}
 		}
-	return item_record_type{};
+	}
+	return null_item;
 }
 
-item_record_type get_weapon(short loot,short level)
+const item_record_type& get_weapon(short loot,short level)
 {
 	if (loot == 0)
-		return item_record_type{};
+		return null_item;
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::OneHandedWeapon, item_variety::TwoHandedWeapon, item_variety::Invalid);
 }
 
-item_record_type get_armor(short loot,short level)
+const item_record_type& get_armor(short loot,short level)
 {
 	if (loot == 0)
-		return item_record_type{};
+		return null_item;
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Armor, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_helm(short loot)
+const item_record_type& get_helm(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Helm, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_gloves(short loot)
+const item_record_type& get_gloves(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Gloves, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_boots(short loot)
+const item_record_type& get_boots(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Boots, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_shield(short loot)
+const item_record_type& get_shield(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Shield, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_potion(short loot)
+const item_record_type& get_potion(short loot)
 {
 	if (get_ran(1,0,80) < 20 * (4 - loot))
 		return pull_item_of_type(loot,loot_min[loot],loot_max[loot] / 2, item_variety::PotionOrMagicItem, item_variety::Invalid, item_variety::Invalid);
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::PotionOrMagicItem, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_scroll(short loot)
+const item_record_type& get_scroll(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::ScrollOrMagicItem, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_missile(short loot)
+const item_record_type& get_missile(short loot)
 {
 	if (get_ran(1,0,2) < 2)
 		return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Arrows, item_variety::ThrownMissile, item_variety::Bow);
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Crossbow, item_variety::Bolts, item_variety::MissileWeapon);
 }
 
-item_record_type get_poison(short loot,short level)
+const item_record_type& get_poison(short loot,short level)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::WeaponPoison, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_wand(short loot)
+const item_record_type& get_wand(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Wand, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_ring(short loot)
+const item_record_type& get_ring(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Ring, item_variety::Invalid, item_variety::Invalid);
 }
 
-item_record_type get_necklace(short loot)
+const item_record_type& get_necklace(short loot)
 {
 	return pull_item_of_type(loot,loot_min[loot],loot_max[loot], item_variety::Necklace, item_variety::Invalid, item_variety::Invalid);
 } 
